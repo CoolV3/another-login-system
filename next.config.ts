f0740521a6947b5c559276313210357eb337893e
Constantin Meier <meierconsti@web.de>
2026-08-13 10:21:11 +0200

    Added cloudflare compactibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    outputFileTracingIncludes: {
+        "/**/*": [
+            "./node_modules/pg-cloudflare/dist/**/*",
+            "./node_modules/pg-cloudflare/esm/**/*",
+        ],
+    },
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
