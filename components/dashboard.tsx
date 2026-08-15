53a9c728cac4261e5bc1d0f65781ee0e737baaf3
Constantin Meier <meierconsti@web.de>
2026-08-15 18:24:58 +0200

    Added a link to the account settings page to the dashboard page.

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 
 export default function Dashboard() {
@@ -7,6 +7,7 @@ export default function Dashboard() {
     return (
         <div className="flex items-center justify-center">
             <h1 className="text-4xl text-center">Here you can code your application.</h1>
+            <Link href="/dashboard/settings" className="bg-yellow-400 px-5 py-3 text-lg rounded-2xl cursor-pointer">Account Settings</Link>
         </div>
     )
 }
